2d87c230be7f5130cc30c222bf7869feaf9a1adf
Jalen Stephens <108702328+Jalen-Stephens@users.noreply.github.com>
2024-02-03 12:23:56 -0500

    Initializing Flask App

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # devfest24
+
+Devfest24 Hackathon Project - Enviromental / Sustainability Track
+
+Authors:
+
+Katie Wimmer
+Jalen Stephens
+
+
+Project Name: 
+
+
+Project Descripition:
---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/dbname'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Add other configuration variables as needed
+
