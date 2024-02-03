0cc95a4dd34e3324b8a3ca5350b9140cb775062c
Jalen Stephens <108702328+Jalen-Stephens@users.noreply.github.com>
2024-02-03 12:58:52 -0500

    Removing the files that aren't inside backend folder

--- app.py
+++
@@ -1,6 +0,0 @@
-import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
--- routes/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from ..models.person import Person
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    # Just an example route
-    return "Hello, Flask App!"
