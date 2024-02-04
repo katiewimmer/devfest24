addc0fea286b96eb7d3c2eb2baa912125c28fabf
katiewimmer <krwim78@gmail.com>
2024-02-03 19:20:43 -0500

    finish server.py

--- backend/server.py
+++ backend/server.py
@@ -1,82 +1,29 @@
 
 """
-Columbia's COMS W4111.001 Introduction to Databases
-Example Webserver
-To run locally:
-    python3 server.py
-Go to http://localhost:8111 in your browser.
-A debugger such as "pdb" may be helpful for debugging.
-Read about it online.
+GreenMeet. server.py file
 """
 import os
-from sqlalchemy import *
-from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, session, g, redirect, Response, abort, url_for
-from datetime import datetime
-import re
-from sqlalchemy.exc import IntegrityError
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
+@app.route('/')
+def index():
+  return render_template("index.html")
 
-
-@app.before_request
-def before_request():
-  """
-  This function is run at the beginning of every web request
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request.
-
-  The variable g is globally accessible.
-  """
-  try:
-    g.conn = engine.connect()
-  except:
-    print("uh oh, problem connecting to database")
-    import traceback; traceback.print_exc()
-    g.conn = None
-
-@app.teardown_request
-def teardown_request(exception):
-  """
-  At the end of the web request, this makes sure to close the database connection.
-  If you don't, the database could run out of memory!
-  """
-  try:
-    g.conn.close()
-  except Exception as e:
-    pass
-
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-#
-# see for routing: https://flask.palletsprojects.com/en/2.0.x/quickstart/?highlight=routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
+@app.route('/loginin/')
+def filmmaker():
+    return render_template('loginin.html')
 
 @app.route('/filmmaker/')
 def filmmaker():
     return render_template('filmmaker.html')
 
+@app.route('/signup/')
+def filmmaker():
+    return render_template('signup.html')
 
-@app.route('/')
-def index():
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  #return render_template("index.html", **context)
-  return render_template("index.html", **context)
 
 #
 # This is an example of a different path.  You can see it at:
@@ -86,26 +33,6 @@ def index():
 # Notice that the function name is another() rather than index()
 # The functions for each app.route need to have different names
 #
-@app.route('/another')
-def another():
-  return render_template("another.html")
-
-
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  params_dict = {"name":name}
-  g.conn.execute(text('INSERT INTO test(name) VALUES (:name)'), params_dict)
-  g.conn.commit()
-  return redirect('/')
-
-
-@app.route('/login')
-def login():
-    abort(401)
-    this_is_never_executed()
-
 
 if __name__ == "__main__":
   import click
